211307e3085dcb152d0b17bbcca43123ea6c508b
Zakir Durumeric <zakird@gmail.com>
2013-08-26 07:59:54 -0400

    adding types

---
+++ src/types.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+#ifndef TYPES_H_
+#define TYPES_H_
+typedef uint32_t ipaddr_n_t; // IPv4 address network order
+typedef uint32_t ipaddr_h_t; // IPv4 address host order
+typedef uint16_t port_n_t; // port network order
+typedef uint16_t port_h_t; // port host order
+typedef unsigned char macaddr_t;
+
+#endif
