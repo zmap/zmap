57127de0e5636a3dbc2e264cdb315aa4cdad93eb
MrPumo <bigpumo@gmail.com>
2014-03-18 18:43:20 +0100

    modified probeok,app_rstr in app_success,app_rcode_str

--- src/probe_modules/module_udp_ntpmonlist.c
+++ src/probe_modules/module_udp_ntpmonlist.c
@@ -238,9 +238,9 @@ void udp_ntpmonlist_process_packet(const u_char *packet, UNUSED uint32_t len, fi
 		fs_add_null(fs, "icmp_type");
 		fs_add_null(fs, "icmp_code");
 		fs_add_null(fs, "icmp_unreach_str");
-		fs_add_uint64(fs, "probeok", app_success);
-		fs_add_string(fs, "app_rstr", (char *) udp_ntpmonlist_response_strings[app_success], 0);
+		fs_add_uint64(fs, "app_success", app_success);
 		fs_add_uint64(fs, "app_rcode",app_success);
+		fs_add_string(fs, "app_rcode_str", (char *) udp_ntpmonlist_response_strings[app_success], 0);
 		fs_add_uint64(fs, "udp_pkt_size", ntohs(udp_hdr->uh_ulen));
 		fs_add_binary(fs, "data", (ntohs(udp_hdr->uh_ulen) - sizeof(struct udphdr)), (void*) &udp_hdr[1], 0);
 
@@ -263,9 +263,9 @@ void udp_ntpmonlist_process_packet(const u_char *packet, UNUSED uint32_t len, fi
 		} else {
 			fs_add_string(fs, "icmp_unreach_str", (char *) "unknown", 0);
 		}
-		fs_add_uint64(fs, "probeok", 0);
-		fs_add_null(fs, "app_rstr");
+		fs_add_uint64(fs, "app_success", 0);
 		fs_add_null(fs, "app_rcode");
+		fs_add_null(fs, "app_rcode_str");
 		fs_add_null(fs, "udp_pkt_size");
 		fs_add_null(fs, "data");
 	} else {
@@ -277,9 +277,9 @@ void udp_ntpmonlist_process_packet(const u_char *packet, UNUSED uint32_t len, fi
 		fs_add_null(fs, "icmp_type");
 		fs_add_null(fs, "icmp_code");
 		fs_add_null(fs, "icmp_unreach_str");
-		fs_add_uint64(fs, "probeok", 0);
-		fs_add_null(fs, "app_rstr");
+		fs_add_uint64(fs, "app_success", 0);
 		fs_add_null(fs, "app_rcode");
+		fs_add_null(fs, "app_rcode_str");
 		fs_add_null(fs, "udp_pkt_size");
 		fs_add_null(fs, "data");
 	}
@@ -345,9 +345,9 @@ static fielddef_t fields[] = {
 	{.name = "icmp_type", .type = "int", .desc = "icmp message type"},
 	{.name = "icmp_code", .type = "int", .desc = "icmp message sub type code"},
 	{.name = "icmp_unreach_str", .type = "string", .desc = "for icmp_unreach responses, the string version of icmp_code (e.g. network-unreach)"},
-	{.name = "probeok", .type="int", .desc = "is response considered APPLICATION success"},
-	{.name = "app_rstr", .type = "string", .desc = "for udp_ntpmonlist module: NTP ERR invalid monlist response - NTP NOERR valid monlist response"},
+	{.name = "app_success", .type="int", .desc = "is response considered APPLICATION success"},
 	{.name = "app_rcode", .type = "int", .desc = "for udp_ntpmonlist module:, 0 invalid sequence - 1 valid sequence"},
+	{.name = "app_rcode_str", .type = "string", .desc = "for udp_ntpmonlist module: NTP ERR invalid monlist response - NTP NOERR valid monlist response"},
 	{.name = "udp_pkt_size", .type="int", .desc = "UDP packet lenght"},
 	{.name = "data", .type="binary", .desc = "UDP payload"}
 };
