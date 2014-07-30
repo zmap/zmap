4dd9a40461d38af27fa4cb98181a019c62de9050
David Adrian <davidcadrian@gmail.com>
2014-07-30 10:49:09 -0400

    Include the renamed files

--- src/get_gateway.c
+++ src/get_gateway.c
@@ -24,7 +24,7 @@
 
 
 #if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__)
-#include "get_gateway_bsd.h"
+#include "get_gateway-bsd.h"
 #else // (linux)
-#include "get_gateway_linux.h"
+#include "get_gateway-linux.h"
 #endif
