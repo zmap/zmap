dec6e307ad1d7ea789b660b04714bcfd4245c0a6
David Adrian <davidcadrian@gmail.com>
2014-08-24 21:36:57 -0400

    Lock around setting the color
    Fixes #202
    
    **A Common ZMap Development Story**
    - Zakir sees problem
    - Zakir writes solution for problem
    - Zakir's solution does not fix problem
    - David fixes Zakir's solution

--- lib/logger.c
+++ lib/logger.c
@@ -35,7 +35,27 @@ static const char *log_level_name[] = {
 #define CYAN    "\x1b[36m"
 #define RESET   "\033[0m"
 
-#define COLOR(x) do { if(color) fprintf(log_output_stream, x); } while (0)
+#define COLOR(x) do { if(color) fprintf(log_output_stream, "%s", x); } while (0)
+
+static const char* color_for_level(enum LogLevel level)
+{
+	switch(level) {
+	case ZLOG_FATAL:
+		return RED;
+	case ZLOG_ERROR:
+		return MAGENTA;
+	case ZLOG_WARN:
+		return YELLOW;
+	case ZLOG_INFO:
+		return GREEN;
+	case ZLOG_DEBUG:
+		return BLUE;
+	case ZLOG_TRACE:
+		return RESET;
+	default:
+		return RESET;
+	}
+}
 
 static int LogLogVA(enum LogLevel level, const char *loggerName,
 		const char *logMessage, va_list args)
@@ -46,8 +66,11 @@ static int LogLogVA(enum LogLevel level, const char *loggerName,
 	if (log_output_stream == stdout || log_output_stream == stderr) {
 		lock_file(log_output_stream);
 	}
+	if (color) {
+		COLOR(color_for_level(level));
+	}
 	if (level <= log_output_level) {
-		
+
 		const char *levelName = log_level_name[level];
 		struct timeval now;
 		char timestamp[256];
@@ -68,7 +91,9 @@ static int LogLogVA(enum LogLevel level, const char *loggerName,
 		}
 
 	}
-	COLOR(RESET);
+	if (color) {
+		COLOR(RESET);
+	}
 	fflush(log_output_stream);
 	if (log_output_stream == stdout || log_output_stream == stderr) {
 		unlock_file(log_output_stream);
@@ -77,7 +102,6 @@ static int LogLogVA(enum LogLevel level, const char *loggerName,
 }
 
 int log_fatal(const char *name, const char *message, ...) {
-	COLOR(RED);
 	va_list va;
 	va_start(va, message);
 	LogLogVA(ZLOG_FATAL, name, message, va);
@@ -91,7 +115,6 @@ int log_fatal(const char *name, const char *message, ...) {
 }
 
 int log_error(const char *name, const char *message, ...) {
-	COLOR(MAGENTA);	
 	va_list va;
 	va_start(va, message);
 	int ret = LogLogVA(ZLOG_ERROR, name, message, va);
@@ -105,7 +128,6 @@ int log_error(const char *name, const char *message, ...) {
 }
 
 int log_warn(const char *name, const char *message, ...) {
-	COLOR(MAGENTA);
 	va_list va;
 	va_start(va, message);
 	int ret = LogLogVA(ZLOG_WARN, name, message, va);
@@ -118,7 +140,6 @@ int log_warn(const char *name, const char *message, ...) {
 }
 
 int log_info(const char *name, const char *message, ...) {
-	COLOR(GREEN);
 	va_list va;
 	va_start(va, message);
 	int ret = LogLogVA(ZLOG_INFO, name, message, va);
@@ -139,7 +160,6 @@ int log_info(const char *name, const char *message, ...) {
 }
 
 int log_debug(const char *name, const char *message, ...) {
-	COLOR(BLUE);
 	va_list va;
 	va_start(va, message);
 	int ret = LogLogVA(ZLOG_DEBUG, name, message, va);
@@ -197,4 +217,3 @@ size_t dstrftime(char *buf, size_t maxsize, const char *format, double tm)
 	tv.tv_usec = (long) (tm - floor(tm)) * 1000000;
 	return strftime(buf, maxsize, format, localtime((const time_t*) &tv));
 }
-
