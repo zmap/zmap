5b36db1886008f3aebb1bebd9479fd876faee2ad
David Adrian <davidcadrian@gmail.com>
2015-02-09 21:28:39 -0500

    Fix indentation in module upnp

--- src/probe_modules/module_upnp.c
+++ src/probe_modules/module_upnp.c
@@ -34,8 +34,8 @@ int upnp_global_initialize(struct state_conf *state)
     return EXIT_SUCCESS;
 }
 
-int upnp_init_perthread(void* buf, macaddr_t *src,
-        macaddr_t *gw, port_h_t dst_port, __attribute__((unused)) void **arg_ptr)
+int upnp_init_perthread(void* buf, macaddr_t *src, macaddr_t *gw,
+		port_h_t dst_port, __attribute__((unused)) void **arg_ptr)
 {
     memset(buf, 0, MAX_PACKET_SIZE);
     struct ether_header *eth_header = (struct ether_header *) buf;
@@ -52,7 +52,7 @@ int upnp_init_perthread(void* buf, macaddr_t *src,
     char* payload = (char*)(&udp_header[1]);
 
     assert(sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct udphdr)
-                    + strlen(upnp_query) <= MAX_PACKET_SIZE);
+		+ strlen(upnp_query) <= MAX_PACKET_SIZE);
 
     strcpy(payload, upnp_query);
 
@@ -64,126 +64,152 @@ void upnp_process_packet(const u_char *packet,
         __attribute__((unused)) uint32_t len, fieldset_t *fs)
 {
 
-    struct ip *ip_hdr = (struct ip *) &packet[sizeof(struct ether_header)];
-        if (ip_hdr->ip_p == IPPROTO_UDP) {
-                struct udphdr *udp = (struct udphdr *) ((char *) ip_hdr + ip_hdr->ip_hl * 4);
+	struct ip *ip_hdr = (struct ip *) &packet[sizeof(struct ether_header)];
+	if (ip_hdr->ip_p == IPPROTO_UDP) {
+		struct udphdr *udp = (struct udphdr *) ((char *) ip_hdr + ip_hdr->ip_hl * 4);
 
-        char *payload = (char*)(&udp[1]);
-        uint16_t plen = udp->uh_ulen - 8;
+	        char *payload = (char*)(&udp[1]);
+	        uint16_t plen = udp->uh_ulen - 8;
 
-        char *s = malloc(plen+1);
-        //char *t = malloc(plen+1);
-        assert(s);
-            strncpy(s, payload, plen);
-            //strncpy(t, payload, plen);
-            s[plen] = 0;
+	        char *s = malloc(plen+1);
+	        //char *t = malloc(plen+1);
+	        assert(s)
+		strncpy(s, payload, plen);
+		//strncpy(t, payload, plen);
+		s[plen] = 0;
 
-        int is_first = 1;
-        const char *classification= NULL;
-        uint64_t is_success = 0;
+	        int is_first = 1;
+	        const char *classification= NULL;
+	        uint64_t is_success = 0;
 
-        char *server=NULL, *location=NULL, *usn=NULL, *st=NULL, *cachecontrol=NULL, *ext=NULL, *xusragent=NULL,
-                *date=NULL, *agent=NULL;
-        char *pch = strtok(s, "\n");
-        while (pch != NULL) {
-            if (pch[strlen(pch)-1] == '\r') {
-                pch[strlen(pch)-1] = '\0';
-            }
-            if (strlen(pch) == 0) {
-                pch = strtok(NULL, "\n");
-                continue;
-            }
-            // the first pch is always supposed to be an HTTP response
-            if (is_first) {
-                if (strcmp(pch, "HTTP/1.1 200 OK")) {
-                    classification = "no-http-header";      
-                    is_success = 0;
-                    goto cleanup;
-                }
-                is_first = 0;
-                is_success = 1;
-                classification = "ok";
-                pch = strtok(NULL, "\n");
-                continue;
-            }
-            char *value = pch;
-            char *key = strsep(&value, ":");
-            if (value && value[0] == ' ') {
-                value += (size_t)1;
-            }
-            if (!key) {
-                pch = strtok(NULL, "\n");
-                continue;
-            }
-            if (!value) {
-                pch = strtok(NULL, "\n");
-                continue;
-            }
+	        char *server=NULL, *location=NULL, *usn=NULL, *st=NULL,
+				*cachecontrol=NULL, *ext=NULL, *xusragent=NULL,
+				*date=NULL, *agent=NULL;
+	        char *pch = strtok(s, "\n");
+	        while (pch != NULL) {
+			if (pch[strlen(pch)-1] == '\r') {
+				pch[strlen(pch)-1] = '\0';
+			}
+			if (strlen(pch) == 0) {
+				pch = strtok(NULL, "\n");
+				continue;
+			}
+			// the first pch is always supposed to be an HTTP response
+			if (is_first) {
+				if (strcmp(pch, "HTTP/1.1 200 OK")) {
+					classification = "no-http-header";
+					is_success = 0;
+					goto cleanup;
+				}
+				is_first = 0;
+				is_success = 1;
+				classification = "ok";
+				pch = strtok(NULL, "\n");
+				continue;
+			}
+			char *value = pch;
+			char *key = strsep(&value, ":");
+			if (value && value[0] == ' ') {
+				value += (size_t)1;
+			}
+			if (!key) {
+				pch = strtok(NULL, "\n");
+				continue;
+			}
+			if (!value) {
+				pch = strtok(NULL, "\n");
+				continue;
+			}
 
-            if (!strcmp(key, "Server") || !strcmp(key, "server") || !strcmp(key, "SERVER")) {
-                server = strdup(value);
-            } else if (!strcmp(key, "Location") || !strcmp(key, "LOCATION")) {
-                location = strdup(value);
-            } else if (!strcmp(key, "USN")) {
-                usn = strdup(value);
-            } else if (!strcmp(key, "EXT")) {
-                ext = strdup(value);
-            } else if (!strcmp(key, "ST")) {
-                st = strdup(value);
-            } else if (!strcmp(key, "Agent")) {
-                agent = strdup(value);
-            } else if (!strcmp(key, "X-User-Agent")) {
-                xusragent = strdup(value);
-            } else if (!strcmp(key, "date") || !strcmp(key, "Date") || !strcmp(key, "DATE")) {
-                date = strdup(value);
-            } else if (!strcmp(key, "Cache-Control") || !strcmp(key, "CACHE-CONTROL")) {
-                cachecontrol = strdup(value);
-            } else {
-                //log_debug("upnp-module", "new key: %s", key);
-            }
-            pch = strtok(NULL, "\n");
-        }
-cleanup:
-        fs_add_string(fs, "classification", (char*)"none", 0);
-                fs_add_uint64(fs, "success", is_success);
-        if (server)
-            fs_add_string(fs, "server", server, 1);
-        else
-            fs_add_null(fs, "server");
-        if (location)
-            fs_add_string(fs, "location", location, 1);
-        else
-            fs_add_null(fs, "location");
-        if (usn)
-            fs_add_string(fs, "usn", usn, 1);
-        else
-            fs_add_null(fs, "usn");
-        if (st)
-            fs_add_string(fs, "st", st, 1);
-        else
-            fs_add_null(fs, "st");
-        if (ext)
-            fs_add_string(fs, "ext", ext, 1);
-        else
-            fs_add_null(fs, "ext");
-        if (cachecontrol)
-            fs_add_string(fs, "cache-control", cachecontrol, 1);
-        else
-            fs_add_null(fs, "cache-control");
-        if (xusragent)
-            fs_add_string(fs, "x-user-agent", xusragent, 1);
-        else
-            fs_add_null(fs, "x-user-agent");
+			if (!strcmp(key, "Server") || !strcmp(key, "server") || !strcmp(key, "SERVER")) {
+				server = strdup(value);
+			} else if (!strcmp(key, "Location") || !strcmp(key, "LOCATION")) {
+				location = strdup(value);
+			} else if (!strcmp(key, "USN")) {
+				usn = strdup(value);
+			} else if (!strcmp(key, "EXT")) {
+				ext = strdup(value);
+			} else if (!strcmp(key, "ST")) {
+				st = strdup(value);
+			} else if (!strcmp(key, "Agent")) {
+				agent = strdup(value);
+			} else if (!strcmp(key, "X-User-Agent")) {
+				xusragent = strdup(value);
+			} else if (!strcmp(key, "date") || !strcmp(key, "Date") || !strcmp(key, "DATE")) {
+				date = strdup(value);
+			} else if (!strcmp(key, "Cache-Control") || !strcmp(key, "CACHE-CONTROL")) {
+				cachecontrol = strdup(value);
+			} else {
+				//log_debug("upnp-module", "new key: %s", key);
+			}
+			pch = strtok(NULL, "\n");
+	        }
+	cleanup:
+		fs_add_string(fs, "classification", (char*)"none", 0);
+		fs_add_uint64(fs, "success", is_success);
 
-        if (agent)
-            fs_add_string(fs, "agent", agent, 1);
-        else
-            fs_add_null(fs, "agent");
+		if (server) {
+			fs_add_string(fs, "server", server, 1);
+		}
+		else {
+			fs_add_null(fs, "server");
+		}
 
-        if (date)
-            fs_add_string(fs, "date", date, 1);
-        else
-            fs_add_null(fs, "date");
+		if (location) {
+			fs_add_string(fs, "location", location, 1);
+		}
+		else {
+			fs_add_null(fs, "location");
+		}
+
+		if (usn) {
+			fs_add_string(fs, "usn", usn, 1);
+		}
+		else {
+			fs_add_null(fs, "usn");
+		}
+
+		if (st) {
+			fs_add_string(fs, "st", st, 1);
+		}
+		else {
+			fs_add_null(fs, "st");
+		}
+
+		if (ext) {
+			fs_add_string(fs, "ext", ext, 1);
+		}
+		else {
+			fs_add_null(fs, "ext");
+		}
+
+		if (cachecontrol) {
+			fs_add_string(fs, "cache-control", cachecontrol, 1);
+		}
+		else {
+			fs_add_null(fs, "cache-control");
+		}
+
+		if (xusragent) {
+			fs_add_string(fs, "x-user-agent", xusragent, 1);
+		}
+		else {
+			fs_add_null(fs, "x-user-agent");
+		}
+
+		if (agent) {
+			fs_add_string(fs, "agent", agent, 1);
+		}
+		else {
+			fs_add_null(fs, "agent");
+		}
+
+		if (date) {
+			fs_add_string(fs, "date", date, 1);
+		}
+		else {
+			fs_add_null(fs, "date");
+		}
 
                 fs_add_uint64(fs, "sport", ntohs(udp->uh_sport));
                 fs_add_uint64(fs, "dport", ntohs(udp->uh_dport));
@@ -192,40 +218,40 @@ cleanup:
                 fs_add_null(fs, "icmp_code");
                 fs_add_null(fs, "icmp_unreach_str");
 
-                fs_add_binary(fs, "data", (ntohs(udp->uh_ulen) - sizeof(struct udphdr)), (void*) &udp[1], 0);
+        	fs_add_binary(fs, "data", (ntohs(udp->uh_ulen) - sizeof(struct udphdr)), (void*) &udp[1], 0);
 
-        free(s);
+		free(s);
 
         } else if (ip_hdr->ip_p == IPPROTO_ICMP) {
-                struct icmp *icmp = (struct icmp *) ((char *) ip_hdr + ip_hdr->ip_hl * 4);
-                struct ip *ip_inner = (struct ip *) &icmp[1];
-                // ICMP unreach comes from another server (not the one we sent a probe to);
-                // But we will fix up saddr to be who we sent the probe to, in case you care.
-                fs_modify_string(fs, "saddr", make_ip_str(ip_inner->ip_dst.s_addr), 1);
-                fs_add_string(fs, "classification", (char*) "icmp-unreach", 0);
-                fs_add_uint64(fs, "success", 0);
+		struct icmp *icmp = (struct icmp *) ((char *) ip_hdr + ip_hdr->ip_hl * 4);
+		struct ip *ip_inner = (struct ip *) &icmp[1];
+		// ICMP unreach comes from another server (not the one we sent a probe to);
+		// But we will fix up saddr to be who we sent the probe to, in case you care.
+		fs_modify_string(fs, "saddr", make_ip_str(ip_inner->ip_dst.s_addr), 1);
+		fs_add_string(fs, "classification", (char*) "icmp-unreach", 0);
+		fs_add_uint64(fs, "success", 0);
 
-        fs_add_null(fs, "server");
-        fs_add_null(fs, "location");
-        fs_add_null(fs, "usn");
-        fs_add_null(fs, "st");
-        fs_add_null(fs, "ext");
-        fs_add_null(fs, "cache-control");
-        fs_add_null(fs, "x-user-agent");
-        fs_add_null(fs, "agent");
-        fs_add_null(fs, "date");
+		fs_add_null(fs, "server");
+		fs_add_null(fs, "location");
+		fs_add_null(fs, "usn");
+		fs_add_null(fs, "st");
+		fs_add_null(fs, "ext");
+		fs_add_null(fs, "cache-control");
+		fs_add_null(fs, "x-user-agent");
+		fs_add_null(fs, "agent");
+		fs_add_null(fs, "date");
 
-                fs_add_null(fs, "sport");
-                fs_add_null(fs, "dport");
-                fs_add_string(fs, "icmp_responder", make_ip_str(ip_hdr->ip_src.s_addr), 1);
-                fs_add_uint64(fs, "icmp_type", icmp->icmp_type);
-                fs_add_uint64(fs, "icmp_code", icmp->icmp_code);
-                if (icmp->icmp_code <= ICMP_UNREACH_PRECEDENCE_CUTOFF) {
-                        fs_add_string(fs, "icmp_unreach_str", (char *) udp_unreach_strings[icmp->icmp_code], 0);
-                } else {
-                        fs_add_string(fs, "icmp_unreach_str", (char *) "unknown", 0);
-                }
-                fs_add_null(fs, "data");
+		fs_add_null(fs, "sport");
+		fs_add_null(fs, "dport");
+		fs_add_string(fs, "icmp_responder", make_ip_str(ip_hdr->ip_src.s_addr), 1);
+		fs_add_uint64(fs, "icmp_type", icmp->icmp_type);
+		fs_add_uint64(fs, "icmp_code", icmp->icmp_code);
+		if (icmp->icmp_code <= ICMP_UNREACH_PRECEDENCE_CUTOFF) {
+			fs_add_string(fs, "icmp_unreach_str", (char *) udp_unreach_strings[icmp->icmp_code], 0);
+		} else {
+			fs_add_string(fs, "icmp_unreach_str", (char *) "unknown", 0);
+		}
+		fs_add_null(fs, "data");
         } else {
                 fs_add_string(fs, "classification", (char *) "other", 0);
                 fs_add_uint64(fs, "success", 0);
