51daf1b38587e6a320b2a94b26791a66842e5b55
David Adrian <davidcadrian@gmail.com>
2015-05-14 17:18:04 -0400

    Travis: Install cmake, fix json-c build

---
+++ scripts/install_cmake.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+curl -L http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.sh > cmake_installer.sh
+diff <(md5sum cmake-3.2.2-Linux-x86_64.sh) sum
+/bin/bash cmake_installer.sh --prefix /opt --exclude-subdir
--- scripts/install_jsonc.sh
+++ scripts/install_jsonc.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+rm -rf json-c-json-c-0.12-20140410
 curl -L https://github.com/json-c/json-c/archive/json-c-0.12-20140410.tar.gz | tar -xz
 
+# Get rid of pesky -Werror
+cp json_c_new_Makefile.am.inc json-c-json-c-0.12-20140410/Makefile.am.inc
+
+# Actually build the damn thing
 cd json-c-json-c-0.12-20140410
 ./autogen.sh
 ./configure
 make
-sudo make install
+
+# Install it
+#sudo make install
