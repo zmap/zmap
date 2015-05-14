25b30cae5e29fa198487db0319668fbf8030e54f
David Adrian <davidcadrian@gmail.com>
2015-05-14 18:18:03 -0400

    More Travis CI fixes

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 curl -L http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.sh > cmake_installer.sh
-diff <(md5sum cmake_installer.sh) cmake_install_checksum
+diff <(md5sum cmake_installer.sh) ./scripts/cmake_install_checksum
 /bin/bash cmake_installer.sh --prefix /opt --exclude-subdir
