8ccd658b8147d8199272b31b6e66804aa155c6b9
David Adrian <davidcadrian@gmail.com>
2015-05-14 18:31:14 -0400

    Fixup path in install_jsonc

--- scripts/install_jsonc.sh
+++ scripts/install_jsonc.sh
@@ -3,7 +3,7 @@ rm -rf json-c-json-c-0.12-20140410
 curl -L https://github.com/json-c/json-c/archive/json-c-0.12-20140410.tar.gz | tar -xz
 
 # Get rid of pesky -Werror
-cp json_c_new_Makefile.am.inc json-c-json-c-0.12-20140410/Makefile.am.inc
+cp ./scripts/json_c_new_Makefile.am.inc json-c-json-c-0.12-20140410/Makefile.am.inc
 
 # Actually build the damn thing
 cd json-c-json-c-0.12-20140410
