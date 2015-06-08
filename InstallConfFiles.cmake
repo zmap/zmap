8894423c956e83c6cd42610a9d036b60e428008c
David Adrian <davidcadrian@gmail.com>
2015-06-08 09:32:48 -0400

    Update install conf script for CONFIG_DESTINATION

--- InstallConfFiles.cmake
+++ InstallConfFiles.cmake
@@ -1,7 +1,7 @@
 if(NOT EXISTS "/etc/zmap/blacklist.conf")
-   file(COPY "${PROJECT_SOURCE_DIR}/conf/blacklist.conf" DESTINATION "/etc/zmap/blacklist.conf")
+   file(COPY "${PROJECT_SOURCE_DIR}/conf/blacklist.conf" DESTINATION "${CONFIG_DESTINATION}/blacklist.conf")
 endif()
 
 if(NOT EXISTS "/etc/zmap/zmap.conf")
-   file(COPY "${PROJECT_SOURCE_DIR}/conf/zmap.conf" DESTINATION "/etc/zmap/zmap.conf")
+   file(COPY "${PROJECT_SOURCE_DIR}/conf/zmap.conf" DESTINATION "${CONFIG_DESTINATION}/zmap.conf")
 endif()
