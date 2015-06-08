e9b28f7bc8b20baab237da926c097327cda20c6a
Zakir Durumeric <zakird@gmail.com>
2015-06-08 10:50:15 -0400

    adding copy right statements to top of every file

--- src/recv.c
+++ src/recv.c
@@ -5,6 +5,7 @@
  * use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
+
 #include "recv.h"
 
 #include <assert.h>
--- src/ztee.c
+++ src/ztee.c
@@ -5,6 +5,7 @@
 * use this file except in compliance with the License. You may obtain a copy
 * of the License at http://www.apache.org/licenses/LICENSE-2.0
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
