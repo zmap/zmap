e53c2737a8939e208260d20708021b55ca70b0e2
Zakir Durumeric <zakird@gmail.com>
2015-09-11 16:47:06 -0700

    adding ip_fragements into recv_count and fixing some identation inconsistencies

--- src/state.h
+++ src/state.h
@@ -118,8 +118,8 @@ struct state_conf {
 	int num_retries;
 	uint64_t total_allowed;
 	uint64_t total_disallowed;
-    int max_sendto_failures;
-    float min_hitrate;
+	int max_sendto_failures;
+	float min_hitrate;
 #ifdef PFRING
 	struct {
 		pfring_zc_cluster *cluster;
@@ -141,7 +141,7 @@ struct state_send {
 	uint32_t sent;
 	uint32_t blacklisted;
 	uint32_t whitelisted;
-    int warmup;
+	int warmup;
 	int complete;
 	uint32_t first_scanned;
 	uint32_t targets;
@@ -166,6 +166,9 @@ struct state_recv {
 	uint32_t cooldown_unique;
 	// valid responses NOT classified as "success"
 	uint32_t failure_total;
+	// how many packets did we receive that were marked as being the first
+	// fragment in a stream
+	uint32_t ip_fragments;
 
 	int complete;  // has the scanner finished sending?
 	double start;  // timestamp of when recv started
