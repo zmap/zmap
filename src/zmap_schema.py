b0fc5ffd860d01dd361aa23014f5bee1cc9bd3de
Zakir Durumeric <zakird@gmail.com>
2015-09-22 10:50:06 -0400

    Adding DNS schema. This needs validation

--- src/zmap_schema.py
+++ src/zmap_schema.py
@@ -36,3 +36,60 @@ zmap_upnp = Record({
 })
 
 register_schema("zmap-upnp", zmap_upnp)
+
+
+dns_question = SubRecord({
+    "name":String(),
+    "qtype":Integer(),
+    "qtype_str":String(),
+    "qclass":Integer(),
+})
+
+dns_answer = SubRecord({
+  "name":String(),
+  "type":Integer(),
+  "class":Integer(),
+  "ttl":Integer(),
+  "rdlength":Integer(),
+  "rdata_is_parsed":Integer(),
+  "rdata":String(), # hex  
+})
+
+zmap_dns = Record({
+        "qr":Short(),
+        "rcode":Short(),
+        "classification":String(),
+        "success":Short(),
+        "app_success":Short(),
+        "sport":Short(),
+        "dport":Short(),
+        "len":Integer(),
+        "icmp_responder":String(),
+        "icmp_type":String(),
+        "icmp_code":Integer(),
+        "icmp_unreach_str":String(),
+        "dns_id":Integer(),
+        "dns_rd":Integer(),
+        "dns_tc":Integer(),
+        "dns_aa":Integer(),
+        "dns_opcode":Integer(),
+        "dns_qr":Integer(),
+        "dns_rcode":Integer(),
+        "dns_cd":Integer(),
+        "dns_ad":Integer(),
+        "dns_z":Integer(),
+        "dns_ra":Integer(),
+        "dns_qdcount":Integer(),
+        "dns_ancount":Integer(),
+        "dns_nscount":Integer(),
+        "dns_arcount":Integer(),
+        "dns_questions":ListOf(dns_question),
+        "dns_answers":ListOf(dns_answer),
+        "dns_authorities":ListOf(dns_answer),
+        "dns_additionals":ListOf(dns_answer),
+        "dns_unconsumed_bytes":String(), 
+        "dns_parse_err":Integer(),
+        "raw_data":String(),
+})
+
+register_schema("zmap-dns", zmap_dns)
