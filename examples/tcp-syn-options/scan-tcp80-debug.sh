0ba541f9cbca3ef03bd8239329308a1b180083e2
Markus Sosnowski <m.sosnowski@soscomp.de>
2017-03-25 21:58:21 +0100

    Bash example bugfix

--- examples/tcp-syn-options/scan-tcp80-debug.sh
+++ examples/tcp-syn-options/scan-tcp80-debug.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [ -z $1 ] || [ $1 < 1 ] || [ $1 > 3 ]
+if [ -z $1 ] || [ $1 -lt 1 ] || [ $1 -gt 3 ]
 then
-	printf "Please Provide test host\n\t1) multipath-tcp.org (true mptcp capable,adds mss, drops invalid)\n\t2) baidu.com (ugly middlebox, echos, replaces TS with NOPs, echoes TFO, echoes MPTCP)\n\t3) google.de (TFO), adds MSS, drops invalid)"
+	printf "Please Provide host to test\n\t1) multipath-tcp.org (true mptcp capable,adds mss, drops invalid)\n\t2) baidu.com (ugly middlebox, echos, replaces TS with NOPs, echoes TFO, echoes MPTCP)\n\t3) google.de (TFO), adds MSS, drops invalid)"
 	exit 1
 fi
 
 host=none
 case $1 in
-1)	host=multipath-tcp.org
-2)	host=baidu.com
-3)	host=google.de
+1)  host=multipath-tcp.org ;;
+2)  host=baidu.com ;;
+3)  host=google.de ;;
 *)	printf "Please Provide test host\n\t1) multipath-tcp.org (true mptcp capable,adds mss, drops invalid)\n\t2) baidu.com (ugly middlebox, echos, replaces TS with NOPs, echoes TFO, echoes MPTCP)\n\t3) google.de (TFO), adds MSS, drops invalid)"
-	exit 1
+	exit 1 ;;
 esac
 
 zmap=../../src/zmap
