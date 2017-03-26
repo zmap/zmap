a7a21b72394d88bb20f2c4f0b0f2964d46815061
Markus Sosnowski <m.sosnowski@soscomp.de>
2017-03-26 13:32:49 +0200

    Fix of shell script

--- examples/tcp-syn-options/scan-tcp80.sh
+++ examples/tcp-syn-options/scan-tcp80.sh
@@ -3,15 +3,16 @@
 zmap=../../src/zmap
 ztee=../../src/ztee
 zgrab=../../bin/zgrab
-result="result-$(date)"
+result="result-$(date +'%d-%m-%Y')"
 port=80
 
 mkdir $result
+touch $result/updates-scanopt-complex-v4-p${port}.csv
 
-$zmap -C zmap-TCPv4-options.conf --output-file $result \
-	-l $result/log-scanopt-complex-v4-p${port}.txt \
-	-m $rp/meta-scanopt-complex-v4-p${port}.json \ 
-	-u $rp/updates-scanopt-complex-v4-p${port}.csv
-	--output-filter="success = 1" -p $port $1 \
-	| $ztee -l $result/log-scanopt-complex-ztee-v4-p${port}.txt \
-	$result/scanopt-complex-v4-p${port}.csv
+$zmap -C zmap-TCPv4-options.conf --output-file $result/output-scanopt-complex-v4-p${port}.csv \
+        -l $result/log-scanopt-complex-v4-p${port}.txt \
+        -m $result/meta-scanopt-complex-v4-p${port}.json \
+        -u $result/updates-scanopt-complex-v4-p${port}.csv \
+        --output-filter="success = 1" -p $port $1 \
+        | $ztee -l $result/log-scanopt-complex-ztee-v4-p${port}.txt \
+        $result/scanopt-complex-v4-p${port}.csv
