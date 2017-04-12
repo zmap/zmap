ecf462d32af7322c87a52701dc4a4bd9531123cf
Gustavo Pantuza Coelho Pinto <gustavo.pantuza@corp.globo.com>
2017-04-12 10:20:27 -0300

    ztests compatibility code

---
+++ src/ztopt_compat.c
@@ -0,0 +1,15 @@
+/*
+ * ZMap Copyright 2013 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+#if __GNUC__ < 4
+#error "gcc version >= 4 is required"
+#elif __GNUC_MINOR__ >= 6
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#endif
+
+#include "ztopt.c"
