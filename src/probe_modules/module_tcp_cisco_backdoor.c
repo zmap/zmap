e2f6f0e3e69299ec3b7d92aad0fe6d416ea006d0
ajholland <ajholland77@yahoo.com>
2018-03-18 19:37:52 -0400

    Add space

--- src/probe_modules/module_tcp_cisco_backdoor.c
+++ src/probe_modules/module_tcp_cisco_backdoor.c
@@ -52,8 +52,8 @@ static int synscan_init_perthread(void *buf, macaddr_t *src, macaddr_t *gw,
 	// From Mandiant
 	// 1. To initiate the process, a uniquely crafted TCP SYN packet is sent
 	//	to port 80 of the “implanted” router. It is important to note
-	//that 	the difference between the sequence and acknowledgment numbers
-	//must 	be set to 0xC123D. Also the ACK number doesn’t need to be zero.
+	// that the difference between the sequence and acknowledgment numbers
+	// must be set to 0xC123D. Also the ACK number doesn’t need to be zero.
 
 #define BACKDOOR_SEQ 0x3D120C00
 //#define BACKDOOR_SEQ 0x000C123D // wrong byte order
