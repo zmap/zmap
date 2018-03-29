732e1f1b9735d7434f6a48ef91ae8ae32d661db7
Zakir Durumeric <zakird@gmail.com>
2018-03-29 10:15:41 -0400

    fixing up warnings that have been showing up on linux systems (#507)
    * fixing up warnings that have been showing up on linux systmes
    
    * handle gcc 4 case
    
    * missing one

--- src/topt_compat.c
+++ src/topt_compat.c
@@ -1,6 +1,8 @@
 #if __GNUC__ < 4
 #error "gcc version >= 4 is required"
-#elif __GNUC_MINOR__ >= 6
+#elif __GNUC__ == 4 && __GNUC_MINOR__ >= 6
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#elif __GNUC_MINOR__ >= 4 
 #pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 #endif
 
