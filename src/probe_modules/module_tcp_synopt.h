6116ce2c1eb2f7a5d2c1e57c264e2f33d888e68f
Oliver Gasser <gasser@net.in.tum.de>
2018-04-16 22:37:27 +0200

    tcpopt: remove debugging output

--- src/probe_modules/module_tcp_synopt.h
+++ src/probe_modules/module_tcp_synopt.h
@@ -78,7 +78,6 @@ static inline void tcpsynopt_process_packet_parse(
 	}
 	// safety stop
 	buf[41*2+2] = '\0';
-	printf("options: %s \n", buf);
 	unsigned int j=0;
 	// inspired by https://nmap.org/book/osdetect-methods.html
 	// ts rfc: https://www.ietf.org/rfc/rfc1323.txt
