020a2fa481c8609f1cd99a5f21742296e246ba16
Manuel Schönlaub <manuel.schoenlaub@bitmovin.com>
2019-10-21 14:37:32 +0200

    format.sh

--- src/output_modules/module_mongodb.h
+++ src/output_modules/module_mongodb.h
@@ -4,4 +4,4 @@
 int mongodb_module_init(struct state_conf *conf, char **fields, int fieldlens);
 int mongodb_module_process(fieldset_t *fs);
 int mongodb_module_close(struct state_conf *c, struct state_send *s,
-                      struct state_recv *r);
+			 struct state_recv *r);
