zmap/zmap

commit 0e94a7cd7c3f4fd363af9c39cb882a218439491b

allow hostnames in blacklist up to 255 bytes (#526)

lib/blacklist.c
@@ -104,7 +104,7 @@ void whitelist_prefix(char *ip, int prefix_len)
 
 static int init_from_string(char *ip, int value)
 {
-	int prefix_len = 32;
+	int prefix_len = 256;
 	char *slash = strchr(ip, '/');
 	if (slash) { // split apart network and prefix length
 		*slash = '\0';
@@ -113,7 +113,7 @@ static int init_from_string(char *ip, int value)
 		errno = 0;
 		prefix_len = strtol(len, &end, 10);
 		if (end == len || errno != 0 || prefix_len < 0 ||
-		    prefix_len > 32) {
+		    prefix_len > 256) {
 			log_fatal("constraint",
 				  "'%s' is not a valid prefix length", len);
 			return -1;
@@ -171,8 +171,9 @@ static int init_from_file(char *file, const char *name, int value,
 		if (comment) {
 			*comment = '\0';
 		}
-		char ip[33];
-		if ((sscanf(line, "%32s", ip)) == EOF) {
+		// hostnames can be up to 255 bytes
+		char ip[256];
+		if ((sscanf(line, "%256s", ip)) == EOF) {
 			continue;
 		}
 		if (init_from_string(ip, value)) {

commit 49a5fc7ff5bf482d09b2924640c4eddf3b284da8

Fix incorrect default prefix length for targets
The default prefix length if none is given should be 32 and not 256, as
256 is not a valid prefix length.

Resolves #591.

This bug was introduced by 0e94a7cd7c3f4fd363af9c39cb882a218439491b
in PR #526.

lib/blacklist.c
@@ -104,7 +104,7 @@ void whitelist_prefix(char *ip, int prefix_len)
 
 static int init_from_string(char *ip, int value)
 {
-	int prefix_len = 256;
+	int prefix_len = 32;
 	char *slash = strchr(ip, '/');
 	if (slash) { // split apart network and prefix length
 		*slash = '\0';
@@ -113,7 +113,7 @@ static int init_from_string(char *ip, int value)
 		errno = 0;
 		prefix_len = strtol(len, &end, 10);
 		if (end == len || errno != 0 || prefix_len < 0 ||
-		    prefix_len > 256) {
+		    prefix_len > 32) {
 			log_fatal("constraint",
 				  "'%s' is not a valid prefix length", len);
 			return -1;