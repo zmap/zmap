1869acf3c654fbee66fda85c834a733bc05b72e8
David Adrian <davidcadrian@gmail.com>
2023-03-22 21:50:52 -0600

    add license frontmatter to constants.h

--- src/constants.h
+++ src/constants.h
@@ -1,3 +1,11 @@
+/*
+ * ZMap Copyright 2023 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 #ifndef ZMAP_CONSTANTS_H
 #define ZMAP_CONSTANTS_H
 
