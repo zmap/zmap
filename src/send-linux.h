cc348abf043e8ec59ae2335b80e6901cd93702b3
Phillip Stephens <phillip@cs.stanford.edu>
2023-12-27 10:13:26 -0500

    phillip/batch-packets: Add dynamic packet batching to take advantage of `sendmmsg` on Linux (#751)
    * wip, saving for weekend
    
    * phillip/batch-packets: pushing to try on VM
    
    * finally got this sendmmsg compiling :(
    
    * moved the import of send-linux.c around so it's only linked/compiled on a linux machine
    
    * added error printing to debug
    
    * fixed pointer arithmetic error
    
    * sendmsg working
    
    * saving now while it's working, sendmmsg WORKS
    
    * cleanup and send batch in cleanup, if applicable
    
    * removed unneeded testing code
    
    * phillip/batch-packets: fixed bug, bad logic when indexing into batch
    
    * phillip/batch-packets: cleaned up and brought the sendmmsg error return behavior to send-bsd.h
    
    * phillip/batch-packets: added retries to linux send
    
    * removed concept of batch, not even sure what it does. We'll reuse it for batch send
    
    * phillip/batch-packets: updated manpages
    
    * phillip/batch-packets: updated --help text
    
    * phillip/batch-packets: added default for batch
    
    * phillip/batch-packets: cleaned up and added comments
    
    * phillip/batch-packets: dynamically allocate batch size array
    
    * phillip/batch-packets: removed BATCH_SIZE from send-linux.c
    
    * phillip/batch-packets: noticed defaults are set in state.c
    
    * phillip/batch-packets: PR feedback
    
    * phillip/batch-packets: revert change in pfring.h
    
    * phillip/batch-packets: cleaned up imports and used log_error
    
    * phillip/batch-packets: moved var declaration inside loop
    
    * phillip/batch-packets: re-worked retry logic to be consistent with user expectaitons
    
    * import sort
    
    * batch created with single calloc for cache locality (thanks dadrian)
    
    * add clarifying comment about sendmmsg packet failure behavior
    
    * PR feedback
    
    * added error msg
    
    * compile error

--- src/send-linux.h
+++ src/send-linux.h
@@ -5,14 +5,22 @@
  * use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
-
 #ifndef ZMAP_SEND_LINUX_H
 #define ZMAP_SEND_LINUX_H
 
-#include "../lib/includes.h"
-#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+#include <netinet/ip.h>
 #include <netpacket/packet.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include "../lib/includes.h"
+#include "./send.h"
+
 
 #ifdef ZMAP_SEND_BSD_H
 #error "Don't include both send-bsd.h and send-linux.h"
@@ -21,40 +29,7 @@
 // Dummy sockaddr for sendto
 static struct sockaddr_ll sockaddr;
 
-int send_run_init(sock_t s)
-{
-	// Get the actual socket
-	int sock = s.sock;
-	// get source interface index
-	struct ifreq if_idx;
-	memset(&if_idx, 0, sizeof(struct ifreq));
-	if (strlen(zconf.iface) >= IFNAMSIZ) {
-		log_error("send", "device interface name (%s) too long\n",
-			  zconf.iface);
-		return EXIT_FAILURE;
-	}
-	strncpy(if_idx.ifr_name, zconf.iface, IFNAMSIZ - 1);
-	if (ioctl(sock, SIOCGIFINDEX, &if_idx) < 0) {
-		log_error("send", "%s", "SIOCGIFINDEX");
-		return EXIT_FAILURE;
-	}
-	int ifindex = if_idx.ifr_ifindex;
-
-	// destination address for the socket
-	memset((void *)&sockaddr, 0, sizeof(struct sockaddr_ll));
-	sockaddr.sll_ifindex = ifindex;
-	sockaddr.sll_halen = ETH_ALEN;
-	if (zconf.send_ip_pkts) {
-		sockaddr.sll_protocol = htons(ETHERTYPE_IP);
-	}
-	memcpy(sockaddr.sll_addr, zconf.gw_mac, ETH_ALEN);
-	return EXIT_SUCCESS;
-}
-
-int send_packet(sock_t sock, void *buf, int len, UNUSED uint32_t idx)
-{
-	return sendto(sock.sock, buf, len, 0, (struct sockaddr *)&sockaddr,
-		      sizeof(struct sockaddr_ll));
-}
+int send_run_init(sock_t s);
+int send_batch(sock_t sock, batch_t* batch, int retries);
 
 #endif /* ZMAP_SEND_LINUX_H */
