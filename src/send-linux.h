c93b33406f33cd478c6ec40ece96fa468431af3d
Phillip Stephens <phillip@cs.stanford.edu>
2024-01-18 17:55:38 +0000

    removed unnecessary file descriptor registration and added a wait for free sqe function

--- src/send-linux.h
+++ src/send-linux.h
@@ -41,7 +41,7 @@ static struct sockaddr_ll sockaddr;
 
 // Each thread will send packets using its own liburing variables to avoid needing locks/increasing contention
 __thread struct io_uring ring;
-#define QUEUE_DEPTH 2048 // ring buffer size for liburing's submission queue
+#define QUEUE_DEPTH 128 // ring buffer size for liburing's submission queue
 #define SQ_POLLING_IDLE_TIMEOUT 15000 // how long kernel thread will wait before timing out
 // The io_uring is an async I/O package. In send_packet, the caller passes in a pointer to a buffer. We'll create a submission queue entry (sqe) using this buffer and put it on the sqe ring buffer.
 // However, then we return to the caller which re-uses this buffer. If we didn't copy the buffer into another data structure, we'd lose data.
@@ -56,7 +56,6 @@ __thread struct data_and_metadata* data_arr;
 // points to an open buffer in buf_array
 __thread uint16_t free_buffer_ptr = 0;
 __thread struct io_uring_cqe* cqe;
-__thread int fds[1]; // socket file descriptor
 __thread int cqe_waits = 0;
 // packets_sent tracks how many packets have been sent over the whole scan, for debugging
 __thread uint packets_sent = 0;
@@ -126,24 +125,17 @@ int send_run_init(sock_t s, uint32_t kernel_cpu)
 	params.sq_thread_idle = SQ_POLLING_IDLE_TIMEOUT;
     log_debug("send_init", "Pinning a kernel polling thread for send events to core %u", kernel_cpu);
 	params.flags |= IORING_SETUP_SQ_AFF;
+	params.flags |= IORING_SETUP_SINGLE_ISSUER;
 	params.sq_thread_cpu = kernel_cpu;
     // From manpages - By default, io_uring will interrupt a task running in userspace when a completion event comes in. This is to ensure that completions run in a timely manner. For a lot of use cases, this is overkill and can cause reduced performance
-//    params.flags |= IORING_SETUP_COOP_TASKRUN;
 	// register socket
-	fds[0] = sock;
 	int ret = io_uring_queue_init_params(QUEUE_DEPTH, &ring, &params);
 	if (ret < 0) {
 		// TODO Phillip handle error
-		fprintf(stderr, "Error initializing io_uring: %s\n", strerror(-ret));
-	}
-	ret = io_uring_register_files(&ring, fds, 1);
-	if (ret < 0) {
-		// TODO Phillip handle error
-		log_fatal("send_init", "Error registering file descriptor: %s/%d", strerror(-ret), ret);
+		log_fatal("send_init", "Error initializing io_uring: %s\n", strerror(-ret));
 	}
 
 	// Once the SQE's are submitted, the memory can be re-used according to the docs for liburing
-	// TODO Phillip trying to get polling working
         data_arr = calloc(QUEUE_DEPTH, sizeof(struct data_and_metadata));
 	log_warn("io_uring_setup", "size of sqe = %d, size of cqe=%d", params.sq_entries, params.cq_entries);
     print_ring_features();
@@ -180,22 +172,17 @@ int send_packet(sock_t sock, void *buf, int len, UNUSED uint32_t idx)
 		has_cleared++;
 		// clear any available cqe's from the ring
 		int cqes_cleared = clear_cqe_ring();
+//		log_warn("send", "cleared %d cqes", cqes_cleared);
 		to_submit -= cqes_cleared;
+		// wait for space in ring buffer to open up
+		io_uring_sqring_wait(&ring);
 		// reattempt to get a new sqe
 		sqe = io_uring_get_sqe(&ring);
-		if (!sqe) {
-			// sqe unavailable, kernel needs more time to send packets
-			usleep(1000);
-		}
-
 	}
-	// since we registered the file descriptor, we can just use the index.
-	// There's only one fd, so index = 0
-	io_uring_prep_sendmsg(sqe, 0, msg, 0);
-	sqe->flags |= IOSQE_FIXED_FILE;
+	io_uring_prep_sendmsg(sqe, sock.sock, msg, 0);
 	// Set the flag IOSQE_CQE_SKIP_SUCCESS to skip completion for successful events
-    sqe->flags |= IOSQE_CQE_SKIP_SUCCESS;
-	io_uring_submit(&ring);
+    	sqe->flags |= IOSQE_CQE_SKIP_SUCCESS;
+        io_uring_submit(&ring);
 	// sqe ready to send, increment arr ptr index to next data
 	free_buffer_ptr = (free_buffer_ptr + 1) % QUEUE_DEPTH;
 	packets_sent++;
@@ -211,6 +198,7 @@ int send_run_cleanup(void) {
 
 	clear_cqe_ring();
 	// free up data structures
+	log_warn("send_run", "waited for sqe entry %d times", cqe_waits);
 	free(data_arr);
 	io_uring_queue_exit(&ring);
 	return 0;
