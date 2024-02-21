f08993d0ed6ac37e6928179bcedbfde07c619924
Timothy Noel <tnoelhere@gmail.com>
2024-02-20 20:16:45 -0500

    add support for SYN-ACK retransmits

--- src/probe_modules/module_ja4ts.c
+++ src/probe_modules/module_ja4ts.c
@@ -14,9 +14,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <assert.h>
+#include <math.h>
+
+#include "cachehash.h"
 
 #include "../../lib/includes.h"
 #include "../../lib/logger.h"
+#include "../../lib/blocklist.h"
 #include "../fieldset.h"
 #include "probe_modules.h"
 #include "packet.h"
@@ -33,17 +37,103 @@
 #define TCP_OPTION_END 0
 
 static uint16_t num_source_ports;
+static cachehash *ch = NULL;
+static char *iptables_check = "iptables -C OUTPUT -p tcp -d %s --tcp-flags RST RST -j DROP"; 
+static char *iptables_add = "iptables -A OUTPUT -p tcp -d %s --tcp-flags RST RST -j DROP"; 
+static char *iptables_delete = "iptables -D OUTPUT -p tcp -d %s --tcp-flags RST RST -j DROP"; 
+
+typedef struct ja4t_tuple {
+        int saddr;
+        int daddr;
+        int sport;
+        int dport;
+} ja4t_tuple_t;
+
+typedef struct ja4t_timedata {
+	struct timespec ts;
+	uint16_t window_size;
+	uint16_t mss_value;
+	uint8_t scaling_factor;
+	char *options;
+	char retransmits[256];
+} ja4t_timedata_t;
+
+static int num_of_digits(int n)
+{
+	return ( n==0 ) ? 1 : (int) log10(n)+1;
+}
+
+static int timediff(const struct timespec *time1, const struct timespec *time0) {
+  	return (int) (((time1->tv_sec - time0->tv_sec)
+            + (time1->tv_nsec - time0->tv_nsec) / 1000000000.0) * 10);
+}
+
+static install_iptable_rules() {
+        char cidr[50];
+	char command[500];
+        bl_cidr_node_t *b = get_allowlisted_cidrs();
+        if (b) {
+                do {
+			memset(command, 0, 500);
+			cidr[0] = '\0';
+
+                        struct in_addr addr;
+                        addr.s_addr = b->ip_address;
+                        sprintf(cidr, "%s/%i", inet_ntoa(addr), b->prefix_len);
+			sprintf(command, iptables_check, cidr);
+
+			int ret = system(command);
+			printf("%s - %d \n", command, ret);
+			if (ret == 256) {
+			        memset(command, 0, 500);
+			    	sprintf(command, iptables_add, cidr);
+			        ret = system(command);
+			}
+                } while (b && (b = b->next));
+        }
+}
+
+static remove_iptable_rules() {
+        char cidr[50];
+	char command[500];
+        bl_cidr_node_t *b = get_allowlisted_cidrs();
+        if (b) {
+                do {
+			memset(command, 0, 500);
+			cidr[0] = '\0';
+
+                        struct in_addr addr;
+                        addr.s_addr = b->ip_address;
+                        sprintf(cidr, "%s/%i", inet_ntoa(addr), b->prefix_len);
+			sprintf(command, iptables_delete, cidr);
+			int ret = system(command);
+                } while (b && (b = b->next));
+        }
+}
 
 static int ja4tscan_global_initialize(struct state_conf *state)
 {
 	num_source_ports =
 	    state->source_port_last - state->source_port_first + 1;
+
+	if (state->dedup_method == DEDUP_METHOD_NONE) 
+	    install_iptable_rules();
+	return EXIT_SUCCESS;
+}
+
+static int ja4tscan_global_cleanup(UNUSED struct state_conf *zconf,
+                              UNUSED struct state_send *zsend,
+                              UNUSED struct state_recv *zrecv)
+{
+	if (zconf->dedup_method == DEDUP_METHOD_NONE) 
+	    remove_iptable_rules();
 	return EXIT_SUCCESS;
 }
 
 static int ja4tscan_init_perthread(void *buf, macaddr_t *src, macaddr_t *gw,
 				   UNUSED void **arg_ptr)
 {
+        ch = cachehash_init(10000, NULL);
 	struct ether_header *eth_header = (struct ether_header *)buf;
 	make_eth_header(eth_header, src, gw);
 	struct ip *ip_header = (struct ip *)(&eth_header[1]);
@@ -67,8 +157,8 @@ static int ja4tscan_make_packet(void *buf, size_t *buf_len, ipaddr_n_t src_ip,
 	struct tcphdr *tcp_header = (struct tcphdr *)(&ip_header[1]);
 	uint32_t tcp_seq = validation[0];
 
-	uint8_t *tcp_options =
-	    (uint8_t *)(&tcp_header[1]); // Points to the start of TCP options
+	//uint8_t *tcp_options =
+	//    (uint8_t *)(&tcp_header[1]); // Points to the start of TCP options
 
 	ip_header->ip_src.s_addr = src_ip;
 	ip_header->ip_dst.s_addr = dst_ip;
@@ -163,13 +253,38 @@ static int ja4tscan_validate_packet(const struct ip *ip_hdr, uint32_t len,
 
 static void ja4tscan_process_packet(const u_char *packet, UNUSED uint32_t len,
 				    fieldset_t *fs, UNUSED uint32_t *validation,
-				    UNUSED struct timespec ts)
+				    struct timespec ts)
 {
 	struct ip *ip_hdr = get_ip_header(packet, len);
 	assert(ip_hdr);
 	if (ip_hdr->ip_p == IPPROTO_TCP) {
 		struct tcphdr *tcp = get_tcp_header(ip_hdr, len);
 		assert(tcp);
+
+		ja4t_tuple_t t = {.saddr = ip_hdr->ip_src.s_addr, .daddr = ip_hdr->ip_dst.s_addr, .sport = tcp->th_sport, .dport = tcp->th_dport };
+		ja4t_timedata_t *timedata = cachehash_get(ch, &t, sizeof(ja4t_tuple_t));
+
+		if (timedata != NULL) {
+		    int diff = timediff(&ts, &timedata->ts);
+		    if (tcp->th_flags & TH_RST) { 
+		        snprintf(timedata->retransmits + strlen(timedata->retransmits), num_of_digits(diff)+3, "R%d-", diff);
+		    } else {
+		        snprintf(timedata->retransmits + strlen(timedata->retransmits), num_of_digits(diff)+2, "%d-", diff);
+	 	    }
+		    timedata->ts.tv_sec = ts.tv_sec;
+		    timedata->ts.tv_nsec = ts.tv_nsec;
+		} else {
+		    timedata = malloc(sizeof(ja4t_timedata_t));
+	            timedata->ts.tv_sec = ts.tv_sec;
+	            timedata->ts.tv_nsec = ts.tv_nsec;
+		    timedata->retransmits[0] = '\0';
+		    timedata->options = malloc(sizeof(char) * 100);
+		    timedata->options[0] = '\0';
+		    timedata->window_size = 0;
+		    timedata->scaling_factor = 0;
+		    timedata->mss_value = 0;
+		    cachehash_put(ch, &t, sizeof(ja4t_tuple_t), (void *)timedata);
+		}
 
 		// JA4TS IMPLEMENTATION
 		// Pointer to the start of TCP options
@@ -182,7 +297,7 @@ static void ja4tscan_process_packet(const u_char *packet, UNUSED uint32_t len,
 		int buffer_size = options_length;
 
 		// PARSE OPTIONS
-		int remaining_length = options_length;
+		uint8_t remaining_length = options_length;
 		int offset = 0;
 
 		// initialize variables
@@ -248,60 +363,64 @@ static void ja4tscan_process_packet(const u_char *packet, UNUSED uint32_t len,
 			}
 		}
 
-		char option_kinds_str[100];
-		option_kinds_str[0] = '\0';
-
-		for (int i = 0; i < num_option_kinds; i++) {
+		// b: TCP Parameters
+		// Options are processed only for the first SYN-ACK
+		// For retransmissions and RST, we use the cached value
+		if (strlen(timedata->options) == 0) {
+		    for (int i = 0; i < num_option_kinds; i++) {
 			// Convert the current option kind to a string
 			char option_kind_str[5];
 			snprintf(option_kind_str, sizeof(option_kind_str), "%d",
 				 option_kinds[i]);
 
 			// If the option_kinds_str is not empty, append a hyphen separator
-			if (option_kinds_str[0] != '\0') {
-				strncat(option_kinds_str, "-",
-					sizeof(option_kinds_str) -
-					    strlen(option_kinds_str) - 1);
+			if (timedata->options[0] != '\0') {
+				strncat(timedata->options, "-", 1);
 			}
 
 			// Append the current sorted option kind
-			strncat(option_kinds_str, option_kind_str,
-				sizeof(option_kinds_str) -
-				    strlen(option_kinds_str) - 1);
-		}
+			strncat(timedata->options, option_kind_str, strlen(option_kind_str));
+		    }
+
+		    if (strlen(timedata->options) == 0)
+		        snprintf(timedata->options, 3, "%s", "00");
+	 	}
 
 		// a: window size
-		uint16_t window_size = ntohs(tcp->th_win);
-
-		// b: TCP Parameters
-		char option_kinds_str_fmt[100];
-		if (strlen(option_kinds_str) == 0) {
-			snprintf(option_kinds_str_fmt,
-				 sizeof(option_kinds_str_fmt), "%s", "00");
-		} else {
-			snprintf(option_kinds_str_fmt,
-				 sizeof(option_kinds_str_fmt), "%s",
-				 option_kinds_str);
-		}
+		// Window size is processed from the first SYN-ACK
+		if (timedata->window_size == 0) 
+		    timedata->window_size = ntohs(tcp->th_win);
+		if (timedata->mss_value == 0) 
+		    timedata->mss_value = mss_value;
+		if (timedata->scaling_factor == 0) 
+		    timedata->scaling_factor = scaling_factor;
 
 		// Calculate the required size for ja4ts_str
 		int required_size =
-		    snprintf(NULL, 0, "%u", window_size) +
-		    strlen(option_kinds_str_fmt) + // For b: TCP Parameters
+		    snprintf(NULL, 0, "%u", timedata->window_size) +
+		    strlen(timedata->options) + // For b: TCP Parameters
 		    strlen("_") +		   // Separator between b and c
-		    sizeof(mss_value) +		   // For c: MSS mss_value
+		    num_of_digits(timedata->mss_value) +		   // For c: MSS mss_value
 		    strlen("_") +		   // Separator between c and d
-		    sizeof(scaling_factor) +	   // For d: Window Scale
-		    strlen("_") +		   // Separator between d and e
-		    sizeof("00") + 1; // For e: Time since last synack
+		    num_of_digits(timedata->scaling_factor) + 2;  // For d: Window Scale
+		
+		int extra_room = 0;		
+		if ((timedata != NULL) && (strlen(timedata->retransmits) > 0))
+		    extra_room = strlen(timedata->retransmits);
 
 		// Allocate memory for ja4ts_str
-		char *ja4ts_str = (char *)malloc(required_size);
+		char *ja4ts_str = (char *)malloc(required_size + extra_room);
 
 		// Construct ja4ts_str
-		snprintf(ja4ts_str, required_size, "%u_%s_%02u_%02u",
-			 window_size, option_kinds_str_fmt, mss_value,
-			 scaling_factor);
+		snprintf(ja4ts_str, required_size, "%u_%s_%02u_%d",
+			 timedata->window_size, timedata->options, timedata->mss_value,
+			 timedata->scaling_factor);
+	
+		// Allocate extra space to include retransmits
+		if (extra_room > 0) {
+		    printf ("adding @:%d, extra_room:%d, retransmit:%s\n", required_size, extra_room, timedata->retransmits);
+		    snprintf(ja4ts_str + strlen(ja4ts_str), extra_room+1, "_%s", timedata->retransmits);
+		}
 
 		log_debug("ja4ts", "JA4TS: %s", ja4ts_str);
 		log_debug("ja4ts", "IP: %s",
@@ -368,7 +487,7 @@ probe_module_t module_ja4ts = {
     .print_packet = &synscan_print_packet,
     .process_packet = &ja4tscan_process_packet,
     .validate_packet = &ja4tscan_validate_packet,
-    .close = NULL,
+    .close = &ja4tscan_global_cleanup,
     .helptext = "Probe module that sends a TCP SYN packet to a specific "
 		"port. Possible classifications are: synack and rst. A "
 		"SYN-ACK packet is considered a success and a reset packet "
