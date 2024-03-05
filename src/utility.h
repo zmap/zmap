22cbc806e11ee5d2fe70ea5b36adfa1d598fc7ce
Phillip Stephens <phillip@cs.stanford.edu>
2024-03-05 22:02:19 +0000

    added context around why we're adding strlcpy

--- src/utility.h
+++ src/utility.h
@@ -21,7 +21,8 @@
 
 void parse_source_ip_addresses(char given_string[]);
 in_addr_t string_to_ip_address(char *t);
-#if defined(__linux__)
+
+#if defined(__linux__) // BSD has an implementation of strlcpy, but linux doesn't by default
 size_t strlcpy(char *dst, const char *src, size_t siz);
 #endif // linux
 
