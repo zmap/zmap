297616672e9693ae6ca69f62846733436bad5d22
phillip-stephens <phillip@cs.stanford.edu>
2024-11-28 08:06:26 -0500

    Revert "enable monitor thread and don't print packets on dryrun, for testing"
    This reverts commit c8bb5128dbd8835266f480487804339f095788b5.

--- src/zmap.c
+++ src/zmap.c
@@ -217,8 +217,7 @@ static void start_zmap(void)
 	uint32_t cpu = 0;
 	pthread_t *tsend, trecv, tmon;
 	int r;
-	// TODO remove
-//	if (!zconf.dryrun) {
+	if (!zconf.dryrun) {
 		recv_arg_t *recv_arg = xmalloc(sizeof(recv_arg_t));
 		recv_arg->cpu = zconf.pin_cores[cpu % zconf.pin_cores_len];
 		cpu += 1;
@@ -234,7 +233,7 @@ static void start_zmap(void)
 			}
 			pthread_mutex_unlock(&recv_ready_mutex);
 		}
-//	}
+	}
 #ifdef PFRING
 	pfring_zc_worker *zw = pfring_zc_run_balancer(
 	    zconf.pf.queues, &zconf.pf.send, zconf.senders, 1,
@@ -264,19 +263,18 @@ static void start_zmap(void)
 	}
 	log_debug("zmap", "%d sender threads spawned", zconf.senders);
 
-	// TODO - remove this, this is just so we print stats during dryruns
-//	if (!zconf.dryrun) {
+	if (!zconf.dryrun) {
 		monitor_init();
 		mon_start_arg_t *mon_arg = xmalloc(sizeof(mon_start_arg_t));
 		mon_arg->it = it;
 		mon_arg->recv_ready_mutex = &recv_ready_mutex;
 		mon_arg->cpu = zconf.pin_cores[cpu % zconf.pin_cores_len];
-		int m = pthread_create(&tmon, NULL, start_mon, mon_arg);
-		if (m != 0) {
+		int r = pthread_create(&tmon, NULL, start_mon, mon_arg);
+		if (r != 0) {
 			log_fatal("zmap", "unable to create monitor thread");
 			exit(EXIT_FAILURE);
 		}
-//	}
+	}
 
 #ifndef PFRING
 	drop_privs();
@@ -297,8 +295,7 @@ static void start_zmap(void)
 	log_debug("zmap", "send queue flushed");
 #endif
 	// no receiving or monitoring thread is started in dry run mode
-	// TODO remove
-//	if (!zconf.dryrun) {
+	if (!zconf.dryrun) {
 		r = pthread_join(trecv, NULL);
 		if (r != 0) {
 			log_fatal("zmap", "unable to join recv thread");
@@ -312,7 +309,7 @@ static void start_zmap(void)
 				exit(EXIT_FAILURE);
 			}
 		}
-//	}
+	}
 
 	// finished
 	if (zconf.metadata_filename) {
