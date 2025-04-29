49909cdc40670b13c787f45a496e9f6b0f1c03a5
Phillip Stephens <phillip@cs.stanford.edu>
2025-04-29 20:32:38 +0000

    initial dh_make results

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/zmap/issues
+# Bug-Submit: https://github.com/<user>/zmap/issues/new
+# Changelog: https://github.com/<user>/zmap/blob/master/CHANGES
+# Documentation: https://github.com/<user>/zmap/wiki
+# Repository-Browse: https://github.com/<user>/zmap
+# Repository: https://github.com/<user>/zmap.git
---
+++ debian/zmap.doc-base.ex
@@ -0,0 +1,20 @@
+Document: zmap
+Title: Debian zmap Manual
+Author: <insert document author here>
+Abstract: This manual describes what zmap is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/zmap/zmap.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/zmap/zmap.ps.gz
+
+Format: text
+Files: /usr/share/doc/zmap/zmap.text.gz
+
+Format: HTML
+Index: /usr/share/doc/zmap/html/index.html
+Files: /usr/share/doc/zmap/html/*.html
